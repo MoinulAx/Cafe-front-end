e3d599076299c31c402d8bad426f847878cefaec
CSAN7690 <christophersanchez@pursuit.org>
2024-07-22 22:03:07 -0400

    updated CRUD for ProductsCard

--- src/Components/ProductsCard/ProductsCard.jsx
+++ src/Components/ProductsCard/ProductsCard.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './productscard.scss';
 
-const ProductCard = ({ product }) => {
+const ProductCard = ({ product, onEdit, onDelete }) => {
     return (
         <li className="product-card">
-            <img src={product.product_image}  className='product-card__image'/>
+            <img src={product.product_image} className='product-card__image' />
             <h2 className='product-card__name'>{product.product_name}</h2>
             <p className='product-card__price'>Price: ${product.product_price}</p>
             <p className='product-card__quantity'>Quantity: {product.product_quantity}</p>
+            <button className='product-card__button' onClick={onEdit}>Edit</button>
+            <button className='product-card__button' onClick={onDelete}>Delete</button>
             <button className='product-card__button'>Add to Cart</button>
         </li>
     );
@@ -24,6 +26,8 @@ ProductCard.propTypes = {
         product_image: PropTypes.string,
         instock: PropTypes.bool.isRequired,
     }).isRequired,
+    onEdit: PropTypes.func.isRequired,
+    onDelete: PropTypes.func.isRequired
 };
 
 export default ProductCard;
